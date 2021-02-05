fdb815205bba092b26d005d5d605a140c85eded1
Florian LEDOUX <florian.ledoux@synergee.com>
2021-02-05 17:26:20 +0100

    test

--- program/interface-commander/Program.cs
+++ program/interface-commander/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿#nullable enable
+using System;
+using Gtk;
 
 namespace interface_commander
 {
-    class Program
-    {
-        static void Main(string[] args)
+    static class Program {
+ 
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Application.Init ();
+
+            Button btn = new Button("Hello world");
+            btn.Clicked += Hello;
+ 
+            Window window = new Window ("helloWorld");
+            window.DeleteEvent += Delete_event;
+        
+            window.Add(btn);
+            window.ShowAll();
+        
+            Application.Run ();
+ 
+        }
+
+        private static void Delete_event(object obj, DeleteEventArgs args)
+        {
+            Application.Quit();
+        }
+
+        private static void Hello(object? obj, EventArgs args)
+        {
+            Console.WriteLine("Hello World");
+            Application.Quit();
         }
     }
 }
