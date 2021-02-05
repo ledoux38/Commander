ab17680e958002bc41cfaad72bde275e64ec1a5b
Florian LEDOUX <florian.ledoux@synergee.com>
2021-02-05 16:40:16 +0100

    first interface

---
+++ program/interface-commander/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace interface_commander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
