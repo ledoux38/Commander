2765a9c349b7a368ca02f3d7253463c9b715ba8e
Florian LEDOUX <florian.ledoux@synergee.com>
2021-02-25 17:27:23 +0100

    fix

--- program/interface/library/Microcontroller.py
+++ program/interface/library/Microcontroller.py
@@ -2,30 +2,34 @@ from interface.library.Peripheral import GPIO
 
 
 class Card:
-    __slots__ = ('name', 'gpio')
+    __slots__ = ('name', '__gpio')
 
     def __init__(self, name):
         if type(name) is not str:
             raise TypeError(name)
 
-        self.gpio: list = []
+        self.__gpio: list = []
         self.name = name
 
     def get_gpio(self, index: int) -> GPIO:
         print("get")
-        return self.gpio[index]
+        return self.__gpio[index]
+
+    def get_all_gpio(self) -> []:
+        print("get")
+        return self.__gpio
 
     def set_gpio(self, index: int, val: GPIO):
         print("set")
         if type(val) is not GPIO:
             raise TypeError(val)
-        self.gpio[index] = val
+        self.__gpio[index] = val
 
     def add_gpio(self, val: GPIO):
         print("add")
         if type(val) is not GPIO:
             raise TypeError(val)
 
-        self.gpio.append(val)
+        self.__gpio.append(val)
 
     property(get_gpio, add_gpio)
--- program/interface/library/test/test_microcontroller.py
+++ program/interface/library/test/test_microcontroller.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
-from interface.library.ElectronicCard import Card
+from interface.library.Microcontroller import Card
 from interface.library.Peripheral import GPIO, GPIOType
 
 
-class TestElectronicCard(TestCase):
+class Test_microcontroller(TestCase):
 
     def test_new_class_with_good_parameters(self):
         test_object: Card = Card("test")
         self.assertEqual(test_object.name, "test")
-        self.assertEqual(len(test_object.gpio), 0)
+        self.assertEqual(len(test_object.get_all_gpio()), 0)
 
     def test_new_class_with_not_good_parameters(self):
         with self.assertRaises(TypeError):
@@ -23,12 +23,11 @@ class TestElectronicCard(TestCase):
         test_object: Card = Card("test")
         test_object.add_gpio(GPIO(GPIOType.INPUT, "test", True))
         test_object.set_gpio(0, GPIO(GPIOType.INPUT, "test", False))
-        self.assertEqual(len(test_object.gpio), 1)
+        self.assertEqual(len(test_object.get_all_gpio()), 1)
         self.assertEqual(test_object.get_gpio(0).value, False)
 
     def test_get_gpio(self):
         test_object: Card = Card("test")
         val: int = 6566
-        with self.assertRaises(TypeError):
-            test_object.gpio.append(val)
-            print(test_object.gpio)
+        with self.assertRaises(AttributeError):
+            test_object.__gpio.append(val)
