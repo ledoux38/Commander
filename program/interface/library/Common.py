7e274d42c5de64b0799504633cb8befaab3e6664
Florian LEDOUX <florian.ledoux@synergee.com>
2021-03-04 14:27:17 +0100

    add ip and test

--- program/interface/library/Common.py
+++ program/interface/library/Common.py
@@ -16,16 +16,46 @@ class Utils(object):
     @staticmethod
     def Scanner(addr: str, max: int) -> list:
         ip_returned: list = []
-        if Utils.Check_ip_is_valid(addr):
-            min: int = int(addr.split(".")[3])
-            if min > max:
-                raise ValueError(srt(min) + ">" + str(max))
-            for num in range(min, max):
-                ip: str = new_addr + str(num)
-                response = ping(ip)
-                if response is not None and response != False:
-                    ip_returned.append(ip)
-        else:
-            raise ValueError(addr)
+        ip: Ip = Ip(addr)
+        min: int = int(ip.Get_value_by_index(3))
+        if min > max:
+            raise ValueError(str(min) + ">" + str(max))
+        for num in range(min, max):
+            ip.Set_value_by_index(3, num)
+            response = ping(ip.__str__())
+            if response is not None and response != False:
+                ip_returned.append(ip.__str__())
 
         return ip_returned
+
+
+class Ip(object):
+    def __init__(self, ip: str):
+        self.Set_ip(ip)
+
+    def __str__(self) -> str:
+        returned_val: str = ""
+        for index, val in enumerate(self.__ip):
+            if index > 0:
+                returned_val += "." + val
+            else:
+                returned_val += val
+        return returned_val
+
+    def Get_ip(self) -> [str]:
+        return self.__ip
+
+    def Set_ip(self, new_ip: str):
+        if Utils.Check_ip_is_valid(new_ip):
+            self.__ip = new_ip.split(".")
+        else:
+            raise ValueError(new_ip)
+
+    def Set_value_by_index(self, index: int, value: int):
+        value = str(value)
+        self.__ip[index] = value
+
+    def Get_value_by_index(self, index: int) -> str:
+        return self.__ip[index]
+
+    property(Get_ip, Set_ip)
--- program/interface/library/test/test_common.py
+++ program/interface/library/test/test_common.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
-from program.interface.library.Common import Utils
+from program.interface.library.Common import Utils, Ip
 
 
-class Test_common(TestCase):
+class Test_Utils(TestCase):
     def test_check_ip_with_good_parameter(self):
         self.assertEqual(Utils.Check_ip_is_valid('192.168.1.22'), True)
 
@@ -12,6 +12,25 @@ class Test_common(TestCase):
     def test_check_ip_with_bad_parameter_1(self):
         self.assertEqual(Utils.Check_ip_is_valid('192.168.1.'), False)
 
-    def test_scanner_(self):
-        pass
+    def test_Scanner(self):
+        Utils.Scanner('192.168.1.20', 22)
 
+
+
+class Test_Ip(TestCase):
+    def test_ip_object_with_good_parameter(self):
+        val: Ip = Ip('192.168.1.22')
+        self.assertEqual(val.Get_ip(), ['192', '168', '1', '22'])
+
+    def test_check_ip_object_with_bad_parameter(self):
+        with self.assertRaises(ValueError):
+            val: Ip = Ip('192.168.1.tototo')
+
+    def test_ip_object_str(self):
+        val: Ip = Ip('192.168.1.22')
+        self.assertEqual(val.__str__(), '192.168.1.22')
+
+    def test_ip_object_set_val_by_index(self):
+        val: Ip = Ip('192.168.1.22')
+        val.Set_value_by_index(3, 20)
+        self.assertEqual(val.Get_ip(), ['192', '168', '1', '20'])
